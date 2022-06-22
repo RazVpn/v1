f03bf8f53e65be4cf402c1a11ffd3e10ca96b945
RazVpn <102897626+RazVpn@users.noreply.github.com>
2022-06-22 23:06:54 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -50,7 +50,7 @@ MYIP=$(wget -qO- icanhazip.com);
 
 echo -e "${green}CHECKING SCRIPT ACCESS${NC}"
 sleep 2
-IZIN=$(curl https://raw.githubusercontent.com/RazVpn/v1/main/IP | grep $MYIP | awk '{print $4}')
+IZIN=$(curl https://raw.githubusercontent.com/RazVpn/ip/main/ipvps | grep $MYIP | awk '{print $4}')
 if [ $MYIP = $IZIN ]; then
     echo -e ""
     echo -e "${green}ACCESS GRANTED...${NC}"
