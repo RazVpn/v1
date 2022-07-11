d88156c025c5e40df1542e78c95c3c522b1b4b7b
RazVpn <102897626+RazVpn@users.noreply.github.com>
2022-07-11 01:37:51 -0700

    Add files via upload

--- MENU/jinggo.sh
+++ MENU/jinggo.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 clear
-red='\e[1;31m'
-green='\e[0;32m'
-blue='\e[0;34m'
-purple='\e[1;35m'
-blue_b='\e[0;94m'
+back_text='\e[0;47;30m'
+BGBLUE='\e[1;44m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[\e[${number}'
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+cyan='\e[\e[${number}'
+line='\e[91m'
+Lgreen='\e[92m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;\e[${number}'
+LIGHT='\033[0;37m'
 NC='\e[0m'
 
-echo ""           
-echo -e  "${blue_b}        ╦╔═╗╦ ╦╦╦╔═╔═╗╔═╗  ╔═╗╔═╗╦═╗╦╔═╗╔╦╗  "${NC}
-echo -e  "${blue_b}        ║║  ╠═╣║╠╩╗╠═╣╠═╣  ╚═╗║  ╠╦╝║╠═╝ ║   "${NC}
-echo -e  "${blue_b}        ╩╚═╝╩ ╩╩╩ ╩╩ ╩╩ ╩  ╚═╝╚═╝╩╚═╩╩   ╩   "${NC}
-echo ""                                                                                
+# BANNER COLOUR
+banner_colour=$(cat /etc/banner)
+# BANNER
+banner=$(cat /usr/bin/bannerku)
+ascii=$(cat /usr/bin/test)
+result_banner() {
+	figlet -f "$ascii" "$banner"
+}
+clear
+echo ""
+printf "\e[${banner_colour}"
+result_banner                                                                           
                                                                                 
 echo -e  "${red}TYPE 'menu' TO GO TO MAIN OR 'exit' TO LOGOUT"${NC}
 echo -e  ""
